ce70449e46423638f4e649542a7e10cf9caf9f98
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-06-28 23:55:07 -0500

    Agregue la carpeta docs

---
+++ docs/precache-manifest.0bcb6d3e0ea7b86f1da9652f55ecc5ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcd2b01d3af37c502324ef18ac4bc13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52372dd0006a8a51b84e",
+    "url": "/static/css/main.a068c43a.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52372dd0006a8a51b84e",
+    "url": "/static/js/main.695897de.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
